699c5852ef8f22844f10ba655606f936e8e96298
Sugut <sugutvictorine@gmail.com>
2022-11-08 23:16:05 +0300

    commit tree faa8024def6abddd8f5f010456bf45343bd5c8eb

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    
+    #url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  
+    def initialize(str)
+        @str = str
+        @uri =''
+    end
+    def get_response_body
+        @uri = URI.parse(@str)
+        @uri.open.string
+    end
+    def parse_json
+        JSON.parse(Net::HTTP.get_response(URI.parse(@str)).body)
+    end
+end    
